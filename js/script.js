565ff72b13261a384369f30e9a4adf8dce976f04
Andrea Esposito <andexpox@gmail.com>
2021-04-20 18:01:49 +0200

    slider-js

--- js/script.js
+++ js/script.js
@@ -7,10 +7,34 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-            
+            activeImage: 0,
+            arrayImages: [
+                'img/campagna.jpg',
+                'img/cascate.webp',
+                'img/fiori.jpeg',
+                'img/montagne.webp'
+            ],
         },
         methods: {
+            nextImage() {
+                const nextImg = this.activeImage + 1;
 
+                if (nextImg > this.arrayImages.length - 1) {
+                    this.activeImage = 0;
+                } else {
+                    this.activeImage = nextImg;
+                }
+            },
+
+            prevImage() {
+                const prevImg = this.activeImage - 1;
+
+                if (prevImg < 0) {
+                    this.activeImage = this.arrayImages.length - 1;
+                } else {
+                    this.activeImage = prevImg;
+                }
+            }
         }
 
     }
